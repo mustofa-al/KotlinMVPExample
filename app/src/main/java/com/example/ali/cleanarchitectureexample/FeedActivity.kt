dfa1bc975bf7b79386c19f94325b4feccc525414
ali <mustofa@extrainteger.com>
2017-10-02 14:34:26 +0700

    dependency

---
+++ app/src/main/java/com/example/ali/cleanarchitectureexample/FeedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ali.cleanarchitectureexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+    }
+}
